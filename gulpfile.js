62267cc079d952ca103b1a77da13b466c702e4e1
ma4ine <lev@xaxa.digital>
2019-03-27 18:26:32 +0300

    Gulp 4 update

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
 'use strict';
 
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    sourcemaps = require('gulp-sourcemaps');
+const { src, dest, parallel, series, watch } = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
-gulp.task('js', function () {
-    return gulp.src([
-        'js/acf-yandex-map.js',
-        'js/yandex-map.js'
-    ])
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('js'))
-});
+// JS
+function js() {
+	return src(['js/acf-yandex-map.js', 'js/yandex-map.js'], { sourcemaps: true })
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(dest('js', { sourcemaps: '.' }))
+}
 
-gulp.task('default', ['js']);
+// Watcher
+function watcher() {
+	watch(['js/acf-yandex-map.js', 'js/yandex-map.js'], js)
+}
+
+// Task
+exports.js = js;
+exports.watcher = watcher;
+exports.default = series(js, watcher);
